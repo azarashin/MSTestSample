3c9545288ae318ef75b20f5bd535cf5d5d9223a7
azarashin <azarashin1979@gmail.com>
2021-06-27 20:18:20 +0900

    added sample test for selenium firefox.

--- CSUnitTest/UnitTest1.cs
+++ CSUnitTest/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote; 
 
 namespace CSUnitTest
 {
@@ -8,6 +11,12 @@ namespace CSUnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            using (IWebDriver driver = new RemoteWebDriver(firefoxOptions))
+            {
+                driver.Navigate().GoToUrl("https://www.bing.com/");
+                driver.Quit(); 
+            }
         }
     }
 }
